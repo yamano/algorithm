52884a52d74d0f9339ecf5d07419c55d94dacd46
Your Name <yamano1023@gmail.com>
2011-10-31 16:34:18 +0900

    既存のArrayクラスにbubblesortメソッドを追加

--- bubblesort_program.rb
+++ bubblesort_program.rb
@@ -1,13 +1,10 @@
-class Sort
-  def initialize(array)
-    @array = array
-  end
-  def bubble
+class Array
+  def bubblesort
     count=0
-    (0...@array.size-1).each do
-      (0...@array.size-1-count).each do |i|
-        if @array[i] > @array[i+1]
-          @array[i],@array[i+1] = @array[i+1],@array[i]
+    (0...self.size-1).each do
+      (0...self.size-1-count).each do |i|
+        if self[i] > self[i+1]
+          self[i],self[i+1] = self[i+1],self[i]
         end
       end
       count+=1
@@ -18,7 +15,6 @@ end
 if $0 == __FILE__
   NUM=10
   p array = (0...NUM).map{|item| rand(100)}
-  s = Sort.new(array)
-  s.bubble
+  array.bubblesort
   p array
 end
