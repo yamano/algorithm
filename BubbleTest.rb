19fd4be2d9be876694e6ce187c9a8568cf267068
Your Name <yamano1023@gmail.com>
2011-10-31 11:18:01 +0900

    add BubbleSortTest

---
+++ BubbleTest.rb
@@ -0,0 +1,26 @@
+class Sort
+  def initialize(sort)
+    @sort = sort
+  end
+  def bubble()
+    loop do
+      flag = 0
+      (0...@sort.size-1).each do |i|
+        if @sort[i] > @sort[i+1]
+          flag = 1
+          @sort[i],@sort[i+1] = @sort[i+1],@sort[i]
+        end
+      end
+      break if flag == 0
+    end
+  end
+end
+NUM=10
+sort = Array.new(NUM)
+sort.each_index do |i|
+  sort[i] = rand(100)
+  p sort[i]
+end
+s = Sort.new(sort)
+s.bubble()
+p sort
