9f83c9fa9b8bdc348b8d43a10ab6081562e3c9a4
Your Name <yamano1023@gmail.com>
2011-10-31 15:19:35 +0900

    flagを除去と見る回数を減らし高速化

--- bubblesort_program.rb
+++ bubblesort_program.rb
@@ -3,18 +3,18 @@ class Sort
     @sort = sort
   end
   def bubble()
-    loop do
-      flag = 0
-      (0...@sort.size-1).each do |i|
+    count=0
+    (0...@sort.size-1).each do
+      (0...@sort.size-1-count).each do |i|
         if @sort[i] > @sort[i+1]
-          flag = 1
           @sort[i],@sort[i+1] = @sort[i+1],@sort[i]
         end
       end
-      break if flag == 0
+      count+=1
     end
   end
 end
+
 if $0 == __FILE__
   NUM=10
   sort = Array.new(NUM)
