4e6a1e642e09059e6824763343156d81006d7d04
Your Name <yamano1023@gmail.com>
2011-10-31 15:25:48 +0900

    配列であるsortの名前をarrayに変更

--- bubblesort_program.rb
+++ bubblesort_program.rb
@@ -1,13 +1,13 @@
 class Sort
-  def initialize(sort)
-    @sort = sort
+  def initialize(array)
+    @array = array
   end
   def bubble()
     count=0
-    (0...@sort.size-1).each do
-      (0...@sort.size-1-count).each do |i|
-        if @sort[i] > @sort[i+1]
-          @sort[i],@sort[i+1] = @sort[i+1],@sort[i]
+    (0...@array.size-1).each do
+      (0...@array.size-1-count).each do |i|
+        if @array[i] > @array[i+1]
+          @array[i],@array[i+1] = @array[i+1],@array[i]
         end
       end
       count+=1
@@ -17,12 +17,12 @@ end
 
 if $0 == __FILE__
   NUM=10
-  sort = Array.new(NUM)
-  sort.each_index do |i|
-    sort[i] = rand(100)
-    p sort[i]
+  array = Array.new(NUM)
+  array.each_index do |i|
+    array[i] = rand(100)
+    p array[i]
   end
-  s = Sort.new(sort)
+  s = Sort.new(array)
   s.bubble()
-  p sort
+  p array
 end
