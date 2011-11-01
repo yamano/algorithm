59a4da14961a47fbc27b53cee1c17aa9d40e2fe2
Your Name <yamano1023@gmail.com>
2011-11-01 17:47:10 +0900

    add quick_sort_benchmark

--- quick_sort.rb
+++ quick_sort.rb
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-require 'benchmark'
-
 class Array
   #　無限ループする可能性がある
   def quick_sort(&block)
@@ -8,7 +6,7 @@ class Array
       threshold = yield(self)
 
       self.partition {|i| i <= threshold }.map do |array|
-        array.quick_sort(&block)
+        array.quick_sort{|item| array.sample}
       end.flatten
     else
       self
@@ -18,23 +16,11 @@ class Array
   def quick_sort!(&block)
     self.replace(self.quick_sort(&block))
   end
+
 end
 
 if $0 == __FILE__
-  NUM=10
-  p array = NUM.times.map {|item| rand(100) }
-  p array.quick_sort {|array| array.sample }
-
-
-  n =1000000
-  Benchmark.bm(15) do |x|
-    x.report("my_quick_sort:"){
-      array=n.times.map{|item| rand(100)}
-      array.quick_sort{|array| array.sample}
-    }
-    x.report("quick_sort:"){
-      array=n.times.map{|item| rand(100)}
-      array.sort
-      }
-  end
+  NUM = 10
+  p array = NUM.times.map{|item| rand(100)}
+  p array.quick_sort{|array| array.sample}
 end
