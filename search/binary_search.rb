13245f2081f9472f4f7d80793312e2b22f8488e3
yamano <yamano1023@gmail.com>
2011-11-02 13:23:25 +0900

    add binary_search

---
+++ search/binary_search.rb
@@ -0,0 +1,28 @@
+class Array
+  def binary_search(ans)
+    mid = self.size/2
+    if self.size >= 1
+      if self[mid] == ans
+        return mid
+      elsif self[mid] > ans
+        array =  self[0...mid]
+        return array.binary_search(ans)
+      elsif self[mid] < ans
+        array =  self[mid+1...self.size]
+        return array.binary_search(ans) + mid + 1
+      end
+    end
+    self.size
+  end
+end
+
+if $0 == __FILE__
+  NUM = 10
+  p array = NUM.times.map{|iten| rand(10)}.sort
+  p ans = rand(10)
+  if array[array.binary_search(ans)] == ans
+    puts "success"
+  else
+    puts "failue"
+  end
+end
