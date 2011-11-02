3a3b0a0357ef046b4fab2574e94fa561ec727f53
yamano <yamano1023@gmail.com>
2011-11-02 15:55:22 +0900

    name　change

--- search/linear_search_spec.rb
+++ search/linear_search_spec.rb
@@ -4,25 +4,25 @@ require './linear_search'
 describe Array do
   it '探索したい数値が存在すれば位置を返す' do
     array = [1,2,3]
-    ans = 3
-    array.linear_search(ans).should==2
+    target = 3
+    array.linear_search(target).should==2
   end
 
   it '探索したい文字が存在すれば位置を返す' do
     array = %w[a b c]
-    ans = 'c'
-    array.linear_search(ans).should==2
+    target = 'c'
+    array.linear_search(target).should==2
   end
 
   it '探索したい文字が存在しなければnilを返す' do
     array = [1,2,3]
-    ans = 4
-    array.linear_search(ans).should==nil
+    target = 4
+    array.linear_search(target).should==nil
   end
 
   it '配列の要素数が1つでも探索し位置を返す' do
     array = [1]
-    ans = 1
-    array.linear_search(ans).should==0
+    target = 1
+    array.linear_search(target).should==0
   end
 end
