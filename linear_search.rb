9e3a2921f607e085adba02a3acd070e3cb3b7233
Your Name <yamano1023@gmail.com>
2011-11-02 10:05:42 +0900

    add linear_search

---
+++ linear_search.rb
@@ -0,0 +1,22 @@
+class Array
+  def linear_search(ans)
+    self.size.times do |i|
+      if self[i] == ans
+        return i
+      end
+    end
+    return self.size
+  end
+end
+
+if $0 == __FILE__
+  NUM = 10
+  p array = NUM.times.map{|item| rand(10)}
+  p ans = rand(10)
+  if array[array.linear_search(ans)] == ans
+    p "success"
+  else
+    p "failue"
+    # p array[array.linear_search(ans)]
+  end
+end
