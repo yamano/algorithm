5fc9bd0f01ddd36f266331b144c888565204af4e
yamano <yamano1023@gmail.com>
2011-11-08 11:28:10 +0900

    テストのリファクタリングした

--- recursive/gcd_spec.rb
+++ recursive/gcd_spec.rb
@@ -1,19 +1,22 @@
+# -*- coding: utf-8 -*-
 require './gcd'
 
 describe "gcd" do
-  it { gcd(8, 12).should == 4 }
+  context '0が入ってない場合' do
+    [ [    8,  12,  4 ],
+      [  150,  45, 15 ],
+      [ -150,  45, 15 ],
+      [  150, -45, 15 ],
+      [ -150, -45, 15 ],
+      [    3,   5,  1 ],
+    ].each do |a, b, answer|
+      it "gcd(#{a}, #{b}) は #{answer}" do
+        gcd(a, b).should == answer
+      end
+    end
+  end
 
-  it { gcd(150, 45).should == 15 }
-
-  it { expect { gcd(0, 4) }.to raise_exception(ArgumentError) }
-
-  it { gcd(-150, 45).should == 15 }
-
-  it { gcd(150, -45).should == 15 }
-
-  it { gcd(-150, -45).should == 15 }
-
-
-
-
+  context '0が入っている場合' do
+    it { expect { gcd(0, 4) }.to raise_exception(ArgumentError) }
+  end
 end
