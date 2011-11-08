4f211079a2e49e7489c269fd4a0d0d0c269d736d
yamano <yamano1023@gmail.com>
2011-11-08 16:47:08 +0900

    テストのリファクタリング

--- recursive/search_num_of_hirakinaori_spec.rb
+++ recursive/search_num_of_hirakinaori_spec.rb
@@ -1,18 +1,14 @@
 require './search_num_of_hirakinaori'
 
 describe "search_num_of_hirakinaori" do
-  it "" do
-    search_num_of_hirakinaori(*[ 3, 3, 4, 5 ]).should == 3435
-  end
-  it "" do
-    search_num_of_hirakinaori(*[1]).should == 1
-  end
 
-  it "" do
-    search_num_of_hirakinaori(*[1,2,3]).should == nil
-  end
-
-  it "" do
-    search_num_of_hirakinaori(*[1]).should == 1
+  [ [ [ 3, 3, 4, 5 ], 3435],
+    [ [ 1          ], 1   ],
+    [ [ 1, 2, 3, 4 ], nil ],
+    [ [ 1, 2, 3    ], nil ],
+  ].each do |args, answer|
+    it "search_num_of_hirakinaori(*#{args}) is #{answer}" do
+      search_num_of_hirakinaori(*args).should == answer
+    end
   end
 end
