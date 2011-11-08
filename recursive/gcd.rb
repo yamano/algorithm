081e7804617f024a259df77e66da2ad7f22f8fb7
yamano <yamano1023@gmail.com>
2011-11-08 14:59:31 +0900

    実装、テストのリファクタリングした

--- recursive/gcd.rb
+++ recursive/gcd.rb
@@ -1,13 +1,25 @@
-def gcd(a, b)
+# -*- coding: utf-8 -*-
+def gcd(a, b, opts = {})
   raise ArgumentError, "args include zero" if a == 0 or b == 0
 
+  start = opts[:start] || 2
   min = [ a.abs, b.abs ].min
 
-  max = nil
-  (1..min).each do |i|
+  (start..min).each do |i|
     if a % i == 0 and b % i == 0
-      max = i
+      return i * gcd(a / i, b / i, :start => i)
     end
   end
-  max
+  1
+end
+
+def gcd_multi(*args)
+  args = args.dup
+  opts = args.last.is_a?(Hash) ? args.pop : {}
+
+  if args.size >= 2
+    gcd_multi(gcd(args.shift, args.shift), *args)
+  else
+    args.first
+  end
 end
--- recursive/gcd_spec.rb
+++ recursive/gcd_spec.rb
@@ -20,3 +20,18 @@ describe "gcd" do
     it { expect { gcd(0, 4) }.to raise_exception(ArgumentError) }
   end
 end
+
+describe 'gcd_multi' do
+  context '0が入ってない場合' do
+    [ [ [ 12,   8, 24 ], 4 ],
+      [ [ 45, 150, 90 ], 15 ],
+      [ [ 3 ], 3 ],
+   ].each do |args, answer|
+      it { gcd_multi(*args).should == answer}
+    end
+  end
+
+  context '0が入っている場合' do
+    it { expect { gcd_multi(0, 4, 8) }.to raise_exception(ArgumentError) }
+  end
+end
