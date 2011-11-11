b20409fcae8725ab2547a7562048b013a01aceec
yamano <yamano@okitan.nsp.ricoh.co.jp>
2011-11-11 14:49:41 +0900

    add find and delete methods

--- tree/tree.rb
+++ tree/tree.rb
@@ -5,17 +5,59 @@ class Tree_node
   end
   attr_accessor :value, :left, :right
 
+  def get(direction)
+    send("get_#{direction}")
+  end
+
+  def get_right
+    @right ||= self.class.new
+  end
+
+  def get_left
+    @left ||= self.class.new
+  end
+
+  def clear(direction)
+    send("clear_#{direction}")
+  end
+
+  def clear_right
+    @right = nil
+  end
+
+  def clear_left
+    @left = nil
+  end
+
+  def which(value)
+    if value < @value
+      :left
+    else
+      :right
+    end
+  end
+
   def insert_tree(value)
     unless @value
       @value = value
     else
-      if @value > value
-        @left ||= self.class.new
-        @left.insert_tree(value)
-      else
-        @right ||= self.class.new
-        @right.insert_tree(value)
-      end
+      get(which(value)).insert_tree(value)
+    end
+  end
+
+  def >(node)
+    if node.is_a?(Tree_node)
+      self.value > node.value
+    else
+      self.value > node
+    end
+  end
+
+  def <(node)
+    if node.is_a?(Tree_node)
+      self.value < node.value
+    else
+      self.value < node
     end
   end
 
@@ -26,8 +68,46 @@ class Tree_node
       self.value == node
     end
   end
-end
 
+  def find_parent(value)
+    if self == value
+      return [ self, nil ]
+    elsif @value > value
+      if @left
+        node, parent = *@left.find_parent(value)
+        unless parent
+          return node, self
+        else
+          return node, parent
+        end
+      end
+    else
+      if @right
+        node, parent = *@right.find_parent(value)
+        unless parent
+          return node, self
+        else
+          return node, parent
+        end
+      end
+    end
+    nil
+  end
+  
+  def delete_node(value)
+    if ret = find_parent(value)
+      node, parent = *ret
+      parent.delete(node)
+      true
+    else
+      false
+    end
+  end
+  
+  def delete(node)
+    clear(which(node))
+  end
+end
 
 if $0 == __FILE__
   tree_node = Tree_node.new(10)
--- tree/tree_spec.rb
+++ tree/tree_spec.rb
@@ -1,43 +1,73 @@
-# -*- coding: undecided -*-
+# -*- coding: utf-8 -*-
 require './tree.rb'
 
 
 describe Tree_node do
   before do
-    @tree_node = Tree_node.new(10)    
+    @tree_node = Tree_node.new(10)
+    @tree_node.insert_tree(20)
+    @tree_node.insert_tree(15)
+    @tree_node.insert_tree(25)
+    @tree_node.insert_tree(30)
   end
 
-  shared_examples_for 'tree node value' do |value, left, right|
-    it { @tree_node.value.should == value }
-    it { @tree_node.left.should  == left  }
-    it { @tree_node.right.should == right }
-  end
+#Todo custom_matcherを使ってテストを書く。
 
-  it_should_behave_like 'tree node value', 10, nil, nil
-
-  context '' do
-    before do
-      @tree_node.insert_tree(20)
-      @tree_node.insert_tree(5)
+=begin
+  Spec::Matchers.define :matcher do |node, value, left, right| 
+    match do
+      #node.value == value
+      #node.left  == left
+      #node.right == right
     end
-
-    it_should_behave_like 'tree node value', 10, 5, 20
+  end
+ 
+  context 'initialize test' do
+    it { should matcher(@tree_node, 10, nil, nil)}
   end
 
-  context '' do
-    before do
-      @tree_node.insert_tree(20)
-      @tree_node.insert_tree(25)
-      @tree_node.insert_tree(30)
-      @tree_node.insert_tree(35)
+    it { should matcher(@tree_node, 10, nil, 20)}
+    it { should matcher(@tree_node.right, 20, 15, 25)}
+    it { should matcher(@tree_node.right.right, 25, nil, 30)}
+=end
+
+  context 'find_parent test' do
+    it { @tree_node.find_parent(10).should == [ 10, nil ] }
+    it { @tree_node.find_parent(5).should  == nil }
+    it { @tree_node.find_parent(20).should == [ 20, 10 ] }
+    it { @tree_node.find_parent(30).should == [ 30, 25 ] }
+  end
+
+  context 'delete test' do
+    it { @tree_node.delete_node(15).should == true }
+    it { @tree_node.delete_node(5).should  == false }
+    it { @tree_node.delete_node(25).should  == true }
+
+    it '' do
+      @tree_node.delete_node(15)
+      @tree_node.find_parent(20).first.left.should be_nil
     end
-    
-    #ToDo custom_matcherを調べて動的にノードを選択してテストする。
-    it_should_behave_like 'tree node value', 10, nil, 20
-    it { @tree_node.right.right.should == 25 }
-    it { @tree_node.right.right.right.should == 30 }
-    it { @tree_node.right.right.right.right.should == 35 }
-    it { @tree_node.right.right.right.left.should == nil }
+      # @tree_node.find_parent(30).first.left.should 
+    #it { @tree_node.delete_node(25).should == true }
+  end
+end
+
+describe Tree_node do
+  before do
+    @tree_node = Tree_node.new(20)
+    @tree_node.insert_tree(5)
+    @tree_node.insert_tree(30)
+  end
+
+  it '' do
+    @tree_node.delete_node(5)
+    @tree_node.find_parent(20).first.left.should be_nil
+  end
+  
+  it '' do
+    @tree_node.delete_node(30)
+    @tree_node.find_parent(20).first.right.should be_nil
   end
 
 end
+
