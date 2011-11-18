f95f066bd51df2e2be36c7ac3f8ecf1cf458b0e8
yamano <yamano1023@gmail.com>
2011-11-18 10:25:23 +0900

    リファクタリングした

--- tree/binary_tree.rb
+++ tree/binary_tree.rb
@@ -2,7 +2,7 @@ class BinaryTree
   def initialize(value = nil)
     @value = value
   end
-  attr_accessor :value, :left, :right
+  attr_accessor  :value, :left, :right
 
   def get(direction)
     send("get_#{direction}")
@@ -15,7 +15,19 @@ class BinaryTree
   def get_left
     @left ||= self.class.new
   end
+=begin
+  def set(direction, node)
+    send("set_#{direction}(#{node})")
+  end
 
+  def set_right(node)
+    @right = node
+  end
+
+  def set_left(node)
+    @left = node
+  end
+=end
   def clear(direction)
     send("clear_#{direction}")
   end
@@ -71,59 +83,28 @@ class BinaryTree
   def find_parent(value)
     if self == value
       return [ self, nil ]
-    elsif @value > value
-      if @left
-        node, parent = *@left.find_parent(value)
-        unless parent
-          return node, self
-        else
-          return node, parent
-        end
-      end
-    else
-      if @right
-        node, parent = *@right.find_parent(value)
-        unless parent
-          return node, self
-        else
-          return node, parent
-        end
+    elsif (@left && @value > value ) || ( @right && @value <= value )
+      node, parent = *get(which(value)).find_parent(value)
+      unless parent
+        return node, self
+      else
+        return node, parent
       end
     end
     nil
   end
-
-=begin
-  def find_parent(value)
-    if self == value
-      return [ self, nil ]
-      if @left || @right
-        node, parent = *get(which(value)).find_parent(value)
-        p node.value
-        unless parent
-          return node, self
-        else
-          return node, parent
-        end
-      end
-      nil
-    end
-  end
-=end
-
 
   def delete_node(value)
     node, parent = *find_parent(value)
     if node.left
       target_node, target_parent = *self.find_parent(find_target_to_swap(node).value)
       node.value = target_node.value
-      #get(which(target_node.value)) = target_node.left
+      #set(which(target_node.value), target_node.left)
       if target_parent < target_node
         target_parent.right = target_node.left
       elsif
         target_parent.left = target_node.left
       end
-      #swap_delete_node(node, find_target_to_swap(node))
     elsif node.right
       if parent < node
         parent.right = node.right
@@ -152,19 +133,6 @@ class BinaryTree
       self
     end
   end
-  
-  def swap_delete_node(node, target)
-    #node.value, target. = target.value, target.left ||= self.class.new
-  end
-  
-  def swap_node(node)
-    if node.right
-      node = node.right
-    else 
-      node = nil
-    end
-  end
-
 end
 
 if $0 == __FILE__
@@ -179,9 +147,9 @@ if $0 == __FILE__
   tree_node.insert_node(23)
   tree_node.insert_node(28)
   tree_node.insert_node(35)
-  tree_node.delete_node(15)
+  #tree_node.delete_node(15)
   #tree_node.delete_node(30)
-  
+  #p tree_node.set(:right, tree_node.right)
   #b, c = *tree_node.get(tree_node.which(10)).find_parent(10)
   #p d, e = *tree_node.find_parent(10)
   #p b.value, c.value
