043152cf12453dcecf25421f2657aaf9a3234bf4
yamano <yamano1023@gmail.com>
2011-11-21 16:52:00 +0900

    ファイル名変更

--- stack/pattern_match.rb
+++ stack/pattern_match.rb
@@ -1,53 +1,32 @@
 # -*- coding: utf-8 -*-
-class Array  
-  def initialize(char, position)
-    @char = char
-    @position = position
-  end
-  attr_accessor :char, :position
-end
-
 class Pattern_match
   def pattern_match(contents)
     stack = []
     queue = []
-    stack_index = []
-    queue_index = []
-    contents.each_char.with_index do |item ,i|
+    contents.zeach_char.with_index do |item ,i|
       if (item == "(" ) || (item == "{")
-        stack.push(item)
-        stack_index.push(i)
-        #print "s #{stack}\n"
+        stack.push( { char => item , position => i } )
       elsif item == ")"
-        if stack[-1] == "("
-          print "#{stack_index[-1]}番目の \"#{stack[-1]}\" と#{i}番目の \"#{item}\" が対応しています\n"
+        if stack != [] && stack[stack.last].map{|char, position| char} == ["("]
+          print "#{stack[-1].map{|char, position| position}}番目の #{stack[-1].map{|char, position| char}} と [#{i}] 番目の [\"#{item}\"] が対応しています\n"
           stack.pop
-          #print "s #{stack}\n"
-          stack_index.pop
         else
-          queue.push(item)
-          queue_index.push(i)
-          #print "q #{queue}\n"
+          queue.push({item => i})
         end
       elsif item == "}"
-        if stack[-1] == "{"
-          print "#{stack_index[-1]}番目の \"#{stack[-1]}\" と#{i}番目の \"#{item}\" が対応しています\n"
+        if stack != [] && stack[-1].map{|char, position| char} == ["{"]
+          print "#{stack[-1].map{|char, position| position}}番目の #{stack[-1].map{|char, position| char}} と [#{i}] 番目の [\"#{item}\"] が対応しています\n"
           stack.pop
-          #print "s #{stack}\n"
-          stack_index.pop
         else
-          queue.push(item)
-          queue_index.push(i)
-          #print "q #{queue}が\n"
+          queue.push({item => i})
         end
       end
     end
-    stack.each_index do |i|
-      print "#{stack_index[i]}番目の \"#{stack[i]}\" が閉じられていません\n"
-
+    stack.size.times do |i|
+        print "#{stack[i].map{|char, position| position}}番目の #{stack[i].map{|char, position| char}} が閉じられていません\n"
     end
-    queue.each_index do |i|
-      print "#{queue_index[i]}番目の \"#{queue[i]}\" が開かれていません\n"
+    queue.size.times do |i|
+        print "#{queue[i].map{|char, position| position}}番目の #{queue[i].map{|char, position| char}} が開かれていません\n"
     end
   end
 end
