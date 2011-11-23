3089da3b809432c8499c8c127ef0e447650f5d6b
yamano <yamano1023@gmail.com>
2011-11-23 15:27:57 +0900

    add kmp_search

--- search/simple_search.rb
+++ search/simple_search.rb
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 class String
   def simple_search(target)
+    count = 0
     self.size.times do | i |
+      count += 1
       if slice( i, target.size ) == target
-        show_answer(i, target)
+        show_answer_simple(i, target)
         return true
       end
     end
@@ -11,8 +13,56 @@ class String
   false
   end
 
-  def show_answer(i, target)
+  def make_slide_table(target, slide_table)
+    text_index = 1
+    target_index = 0
+    while target.length > text_index
+      if target[text_index] == target[target_index]
+        text_index += 1
+        target_index += 1
+        slide_table[text_index] = target_index
+      elsif target_index == 0
+        text_index += 1
+        slide_table[text_index] = 0
+      else
+        p target_index = slide_table[target_index]
+      end
+    end
+  end
+
+  def kmp_search(target)
+    slide_table = Array.new(target.length)
+    make_slide_table(target, slide_table)
+    p slide_table
+    i = 0
+    j = 0
+    count = 0
+    while i < self.length
+      count += 1
+      if self[ i ] == target[ j ]
+        i += 1
+        j += 1
+        if target.length == j
+          show_answer(i, target, count)
+          return true
+        end
+      elsif j==0
+        i += 1
+      else
+        p j, slide_table[j]
+        j = slide_table[j]
+      end
+    end
+    show_error(target)
+    false
+  end
+  
+  def show_answer_simple(i, target)
     puts "#{self}の#{i + 1}番目に#{target}が見つかりました。"
+  end
+
+  def show_answer(i, target, count)
+    puts "#{self}の#{i + 1 - target.length}番目に#{target}が見つかりました。探索回数は#{count}回です。"
   end
 
   def show_error(target)
@@ -21,5 +71,5 @@ class String
 end
 
 if $0 == __FILE__
-  'abcde'.simple_search('bc')
+  'abcabcabcd'.kmp_search('abcabcd')
 end
--- search/simple_search_spec.rb
+++ search/simple_search_spec.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 require './simple_search.rb'
 
-describe 'simple_search' do
+describe 'search' do
   it { "abcde".simple_search('bc').should == true }
 
   it { "abcde".simple_search('fg').should == false }
@@ -9,4 +9,18 @@ describe 'simple_search' do
   it { "hallo world!".simple_search('world').should == true }
 
   it { "ABCDE".simple_search('bcd').should == false }
+
+  it { "abcdefghi".kmp_search('gh').should == true }
+
+  it { "abcdefghi".kmp_search('fg').should == true }
+
+  it { "abcabckkabcabcdefg".kmp_search('abcabcd').should == true }
+
+  it { "abcabcabcabcdefg".kmp_search('abcabcd').should == true }
+
+  it { "abcabcabcda".kmp_search('abcabcd').should == true }
+
+  it { "ABCDE".kmp_search('bcd').should == false }
+
 end
+
