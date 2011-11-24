518cc66bc98c5afb990207065f90540e2a5d5cf2
yamano <yamano1023@gmail.com>
2011-11-24 15:33:27 +0900

    リファクタリングした

--- search/simple_search.rb
+++ search/simple_search.rb
@@ -1,79 +1,79 @@
 # -*- coding: utf-8 -*-
 class String
-  def simple_search(target)
+  def simple_search( target )
     count = 0
     self.size.times do | i |
       count += 1
       if slice( i, target.size ) == target
-        show_answer(i, target, count)
+        show_answer( i, target, count )
         return true
       end
     end
-  show_error(target)
+  show_error( target )
   false
   end
 
-  def make_slide_table(target, slide_table)
+  def make_slide_table( target, slide_table )
     text_index = 1
     target_index = 0
     while target.length > text_index
-      if target[text_index] == target[target_index]
+      if target[ text_index ] == target[ target_index ]
         text_index += 1
         target_index += 1
-        slide_table[text_index] = target_index
+        slide_table[ text_index ] = target_index
       elsif target_index == 0
         text_index += 1
-        slide_table[text_index] = 0
+        slide_table[ text_index ] = 0
       else
-        target_index = slide_table[target_index]
+        target_index = slide_table[ target_index ]
       end
     end
   end
 
   def kmp_search(target)
-    slide_table = Array.new(target.length)
-    make_slide_table(target, slide_table)
-    i = 0
-    j = 0
+    slide_table = Array.new(target.length, 0)
+    make_slide_table( target, slide_table )
+    text_index = 0
+    target_index = 0
     count = 0
-    while i < self.length
+    while text_index < self.length
       count += 1
-      if self[ i ] == target[ j ]
-        i += 1
-        j += 1
-        if target.length == j
-          show_answer(i - target.length, target, count)
+      if self[ text_index ] == target[ target_index ]
+        text_index += 1
+        target_index += 1
+        if target.length == target_index
+          show_answer( text_index - target.length, target, count )
           return true
         end
-      elsif j==0
-        i += 1
+      elsif target_index == 0
+        text_index += 1
       else
-        j = slide_table[j]
+        target_index = slide_table[ target_index ]
       end
     end
-    show_error(target)
+    show_error( target )
     false
   end
 
-  def make_slide_hash(target, slide_hash)
+  def make_slide_hash( target, slide_hash )
     target.length.times do |i|
-      slide_hash[target[i]] = target.length - i - 1
+      slide_hash[ target[ i ] ] = target.length - i - 1
     end
   end
   
-  def bm_search(target)
+  def bm_search( target )
     slide_hash   = {}
     text_index   = target.length - 1
-    make_slide_hash(target, slide_hash)
+    make_slide_hash( target, slide_hash )
     count = 0
 
     while text_index < self.length
       target_index = target.length - 1
       count += 1
       
-      while self[text_index] == target[target_index]
+      while self[ text_index ] == target[ target_index ]
         if target_index == 0
-          show_answer(text_index, target, count)
+          show_answer( text_index, target, count )
           return true
         end
         text_index   -= 1
@@ -81,11 +81,9 @@ class String
         count += 1
       end
       
-      if slide_hash[self[text_index]]
-        #p "s",slide_hash[self[text_index]]
-        #p "t",target.length - target_index
-        if slide_hash[self[text_index]] > target.length - target_index
-          text_index += slide_hash[self[text_index]]
+      if slide_hash[ self[ text_index ] ]
+        if slide_hash[ self[ text_index ] ] > target.length - target_index
+          text_index += slide_hash[ self[ text_index ] ]
         else
           text_index += target.length - target_index
         end
@@ -94,15 +92,15 @@ class String
       end
 
     end
-    show_error(target)
+    show_error( target )
     false
   end
   
-  def show_answer(i, target, count)
+  def show_answer( i, target, count )
     puts "#{self}の#{i + 1}番目に#{target}が見つかりました。探索回数は#{count}回です。"
   end
 
-  def show_error(target)
+  def show_error( target )
     puts "#{self}の中に#{target}が見つかりませんでした。"
   end
 end
--- search/simple_search_spec.rb
+++ search/simple_search_spec.rb
@@ -2,40 +2,61 @@
 require './simple_search.rb'
 
 describe 'search' do
-  it { "abcde".simple_search('bc').should == true }
 
-  it { "abcde".simple_search('fg').should == false }
+  context "simple_search_test" do
+    
+    it "検索対象が存在する場合" do
+      "hallo world!".simple_search('world').should == true
+    end
 
-  it { "hallo world!".simple_search('world').should == true }
+    it "検索対象が存在しない場合" do
+      "ABCDE".simple_search('bcd').should == false
+    end
 
-  it { "ABCDE".simple_search('bcd').should == false }
-
-  it { "abcdefghi".kmp_search('gh').should == true }
-
-  it { "abcdefghi".kmp_search('fg').should == true }
-
-  it { "abcabckkabcabcdefg".kmp_search('abcabcd').should == true }
-
-  it { "abcabcabcabcdefg".kmp_search('abcabcd').should == true }
-
-  it { "abcabcabcda".kmp_search('abcabcd').should == true }
-
-  it { "ABCDE".kmp_search('bcd').should == false }
-
-  it 'ターゲットにない文字が見つかった場合' do
-    "a glowing gleam growing green.".bm_search('gleam').should == true 
   end
 
-  it "不一致を起こした文字がパターンに含まれる場合" do
-    "i wind ???".bm_search('wind').should == true 
+  context "kmp_search_test" do
+
+    it "検索対象が存在する場合" do
+      "abcdefghi".kmp_search('gh').should == true
+    end
+
+    it { "ababc".kmp_search('abc').should == true }
+    it { "acabc".kmp_search('abc').should == true }
+    it { "abaac".kmp_search('aac').should == true }
+    
+    it "ムダを省く場合" do
+      "abcabcabcda".kmp_search('abcabcd').should == true
+    end
+    
+    it "検索対象が存在しない場合" do
+      "ABCDE".kmp_search('bcd').should == false 
+    end
+
   end
 
-  it "不一致を起こした文字がパターンに複数含まれる場合" do
-    "....baggage on".bm_search('baggage').should == true 
+  context "bm_search_test" do
+    
+    it "ターゲットにない文字が見つかった場合" do
+      "a glowing gleam growing green.".bm_search('gleam').should == true 
+    end
+    
+    it "不一致を起こした文字がパターンに含まれる場合" do
+      "i wind ...".bm_search('wind').should == true 
+    end
+    
+    it "不一致を起こした文字がパターンに複数含まれる場合" do
+      "....baggage on".bm_search('baggage').should == true 
+    end
+    
+    it "検索位置が戻ってしまう場合" do
+      ".antenna.kana".bm_search('kana').should == true 
+    end
+
+    it "検索対象が存在しない場合" do
+      "ABCDE".kmp_search('bcd').should == false
+    end
   end
 
-  it "検索位置が戻ってしまう場合" do
-    ".antenna.kana".bm_search('kana').should == true 
-  end
 end
 
