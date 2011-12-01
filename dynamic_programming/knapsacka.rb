45099f0c084c9d3026d88355d9f717acbd32dcb2
yamano <yamano1023@gmail.com>
2011-12-01 17:40:54 +0900

    何が何個入っているかを出力するように実装を変更した

--- dynamic_programming/knapsacka.rb
+++ dynamic_programming/knapsacka.rb
@@ -7,9 +7,10 @@ class Knapsacka
     @size = size
     @value = value
     # knapsacka_valueは商品の個数とナップザックの対象サイズの二重配列に最高の価値を格納するもの。
-    @knapsacka = Array.new(KNAPSACKA_SIZE_MAX)
+    @knapsacka = []
+    @knapsacka_combination = []
   end
-  attr_accessor :size, :value, :knapsacka
+  attr_accessor :size, :value, :knapsacka, :knapsacka_combination
 
   def calculation_value
     @value.length.times do |i|
@@ -17,26 +18,38 @@ class Knapsacka
         if i == 0
           # ナップザックの対象サイズに1種類の商品がいくつふくまれているか計算し、そこから価値を計算
           @knapsacka[j] = (j + 1) / @size[i] * @value[i]
+          @knapsacka_combination[j] = { @value[i] =>  (j + 1) / @size[i] }
         else
           # 商品の種類を増やしたときに最適解を更新する
           update_knapsacka(i, j)
         end
       end
+
     end
-    @knapsacka[KNAPSACKA_SIZE_MAX - 1]
+    @knapsacka_combination
   end
 
   def update_knapsacka(i, j)
     if j == @size[i] - 1
       new_value = @value[i]
+      if new_value >  @knapsacka[j]
+        @knapsacka[j] = new_value
+        @knapsacka_combination[j].clear
+        @knapsacka_combination[j][@value[i]] = 1
+      end
     elsif j >= @size[i]
-      # 
+      tmp = Marshal.load(Marshal.dump(@knapsacka_combination))
       new_value = @knapsacka[j - @size[i]] + @value[i]
-    else
-      new_value = 0
+      if new_value >  @knapsacka[j]
+        @knapsacka[j] = new_value
+        @knapsacka_combination[j] = tmp[j - @size[i]]
+        if @knapsacka_combination[j][@value[i]]
+           @knapsacka_combination[j][@value[i]] += 1
+        else
+           @knapsacka_combination[j][@value[i]] = 1
+        end
+      end
     end
-    # 商品を増やす前と増やした後で価値の大きい方を最適解として代入
-    @knapsacka[j] = new_value >  @knapsacka[j] ? new_value : @knapsacka[j]
   end
 
 end
@@ -48,9 +61,6 @@ if $0 == __FILE__
 
   ex = Knapsacka.new(size.map{|i| i}, value.map{|i| i})
 
-  p ex.calculation_value
-
-  p ex.knapsacka_value
-
+  ex.calculation_value
 
 end
