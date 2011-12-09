2d1a066e883dc8f833cb45518069734beeeecfc8
yamano <yamano1023@gmail.com>
2011-12-09 11:05:55 +0900

    レビュー反映

--- stack/rpn.rb
+++ stack/rpn.rb
@@ -1,21 +1,56 @@
 # -*- coding: utf-8 -*-
-class String
+class PuzzleSolver
 
-  def calculate_rpn
-    stack = []
-    # 文字列を配列に変換
-    string_array = split("")
+  def solve_puzzle(rpn, target)
+    results = make_rpn_pattern_array(rpn.array).select{ |rpn_pattern| rpn.calculate_rpn(rpn_pattern) == target }
+  end
+
+  def make_rpn_pattern_array(array)
+    # 配列が4つの場合しか想定していないため、それ以外の場合はエラーを返す
+    raise if array.size !=4
+    # 配列の順列のパターンを全てnumber_pattern_arrayに格納
+    number_pattern_array = array.permutation.to_a
+    operator = ["+", "-", "*", "\/"]
+    # operatorの重複ありの組み合わせを全てoperator_pattern_arrayに格納
+    operator_pattern_array = operator.product(operator, operator)
+    rpn_pattern_array = []
+
+    number_pattern_array.each do |number_pattern|
+      operator_pattern_array.each do |operator_pattern|
+        # 4つの数字から作れるパターンを作成
+        rpn_pattern_array.push(number_pattern + operator_pattern)
+        rpn_pattern_array.push(number_pattern[0, 3] + operator_pattern[0, 2] + number_pattern[3, 1] + operator_pattern[2, 1])
+        rpn_pattern_array.push(number_pattern[0, 2] + operator_pattern[0, 1] + number_pattern[2, 2] + operator_pattern[1, 2])
+        rpn_pattern_array.push(number_pattern[0, 2] + operator_pattern[0, 1] + number_pattern[2, 1] + operator_pattern[1, 1] + number_pattern[3, 1] + operator_pattern[2, 1])
+      end
+    end
     
-    string_array.each do |char|
-      if has_number?(char)
-        stack.push(char.to_f)
-      elsif has_operator?(char)
-        operate(char, stack)
+    rpn_pattern_array
+    
+  end
+
+end
+
+class Rpn
+
+  def initialize(array)
+    @array = array
+  end
+  attr_accessor :array
+
+  def calculate_rpn(rpn_pattern)
+    stack = []
+    
+    rpn_pattern.each do |item|
+      if item.kind_of? Integer
+        stack.push(item.to_f)
+      elsif item.scan(/[+\-*\/]/) != []
+        operate(item, stack)
       else
         raise
       end
     end
-    stack[0]  
+    stack[0]
   end
 
   def operate(operator, stack)
@@ -24,80 +59,21 @@ class String
     stack.push(n2.send(operator, n1) )
   end
 
-  def has_number?(char)
-    if char.scan(/\d/) != []
-      true
-    else
-      false
-    end
-  end
-
-  def has_operator?(char)
-    if char.scan(/["+", "\-", "*", "\/"]/) != []
-      true
-    else
-      false
-    end
-  end
-
-  def make_rpn_string_array
-    
-    # 文字列が4つの場合しか想定していないため、それ以外の場合はエラーを返す
-    raise if self.size !=4
-
-    # 文字列を配列に変換し、その順列のパターンを全てnumber_pattern_arrayに格納
-    number_pattern_array = split("").permutation.to_a
-    operator = ["+", "\-", "*", "\/"]
-    # operatorの重複ありの組み合わせを全てoperator_pattern_arrayに格納
-    operator_pattern_array = operator.product(operator, operator)
-    rpn_pattern_array = []
-
-    number_pattern_array.each do |number_pattern|
-      operator_pattern_array.each do |operator_pattern|
-        # 4つの数字から作れる逆ポーランド記法のパターンを作成
-        rpn_pattern_array.push(number_pattern + operator_pattern)
-        rpn_pattern_array.push(number_pattern[0, 3] + operator_pattern[0, 2] + number_pattern[3, 1] + operator_pattern[2, 1])
-        rpn_pattern_array.push(number_pattern[0, 2] + operator_pattern[0, 1] + number_pattern[2, 2] + operator_pattern[1, 2])
-        rpn_pattern_array.push(number_pattern[0, 2] + operator_pattern[0, 1] + number_pattern[2, 1] + operator_pattern[1, 1] + number_pattern[3, 1] + operator_pattern[2, 1])
-      end
-    end
-    
-    rpn_string_array = []
-    # 配列を文字列に変換
-    rpn_pattern_array.each_index do |i|
-      rpn_string_array.push(rpn_pattern_array[i].join(""))
-    end
-
-    rpn_string_array
-    
-  end
-
-  def solve_puzzle(target)
-
-    rpn_pattern_array = make_rpn_string_array
-    results = []
-    
-    rpn_pattern_array.each do |rpn_pattern|
-      if rpn_pattern.calculate_rpn == target
-        results.push(rpn_pattern)
-      end
-    end
-    results
-  end
-
 end
 
 if $0 == __FILE__
 
-  str = "5831+/*"
-  str1 = "1234"
-  str2 = "5831"
+  rpn = Rpn.new([5, 8, 3, 1, "+", "/", "*"])
+  rpn1 = Rpn.new([1, 2, 3, 4])
+  rpn2 = Rpn.new([5, 8, 3, 1])
+
+  solver = PuzzleSolver.new
 
 #  str.split("")
 #  p str.calculate_rpn
-   str1.make_rpn_string_array.uniq.size
+   solver.make_rpn_pattern_array(rpn1.array).uniq.size
 #  p str1.make_rpn_pattern_array.size
 #  p str1.make_rpn_pattern_array.uniq.size
-   p str1.solve_puzzle(10)
+   solver.solve_puzzle(rpn2, 10)
   
 end
--- stack/rpn_spec.rb
+++ stack/rpn_spec.rb
@@ -3,45 +3,78 @@ require "./rpn.rb"
 
 describe "calculate_rpn" do
 
-  before do
-    @str1 = "58+"
-    @str2 = "5831+/*"
-    @str3 = "@"
+  context "想定している数字、記号が入力された場合" do
+    
+    before do
+      @rpn1 = Rpn.new([5, 8, "+"])
+      @rpn2 = Rpn.new([5, 8, 3, 1, "+", "/", "*"])
+      @rpn3 = Rpn.new([1, 2, 3, 4, 5, "+", "+", "+", "+"])
+    end
+    
+    it "5.0 + 8.0 が計算されて 13.0 が出力される" do
+      @rpn1.calculate_rpn(@rpn1.array).should == 13.0
+    end
+    
+    it "5.0 * 8.0 / (3.0 + 1.0) が計算されて 10.0 が出力される" do
+      @rpn2.calculate_rpn(@rpn2.array).should == 10.0
+    end
+
+    it "1.0 + 2.0 + 3.0 + 4.0 + 5.0  が計算されて 15.0 が出力される" do
+      @rpn3.calculate_rpn(@rpn3.array).should == 15.0
+    end
+
   end
 
-  it "5.0 + 8.0 が計算されて 13.0 が出力される" do
-    @str1.calculate_rpn.should == 13.0
-  end
+  context "想定していない記号が入力された場合" do
+    
+    before do
+      @rpn4 = Rpn.new(["@"])
+      @rpn5 = Rpn.new([","])
+    end
+    
+    it "不適切な記号が入力されたらエラーを返す" do
+      expect {@rpn4.calclate_rpn(@rpn4)}.to raise_exception
+      expect {@rpn5.calclate_rpn(@rpn5)}.to raise_exception
+    end
 
-  it "5.0 * 8.0 / (3.0 + 1.0) が計算されて 10.0 が出力される" do
-    @str2.calculate_rpn.should == 10.0
-  end
-
-  it "不適切な記号が入力されたらエラーを返す" do
-    expect {@str3.calclate_rpn}.to raise_exception
   end
 
 end
 
 describe "make_rpn_pattern_array" do
   
-  before do
-    @str = "1234"
-    @str1 = "12345"
+  context "数字4つの場合" do
+
+    before do
+      @rpn1 = Rpn.new([1, 2, 3, 4])
+
+      @solver = PuzzleSolver.new
+    end
+    
+    it '逆ポーランド記法で解ける形で作られる最初のパターンと最後のパターンをテスト' do
+      @solver.make_rpn_pattern_array(@rpn1.array)[0].should == [1, 2, 3, 4, "+", "+", "+"]
+      @solver.make_rpn_pattern_array(@rpn1.array)[-1].should == [4, 3, "/", 2, "/", 1, "/"]
+    end
+    
+    it '4つの数字から逆ポーランド記法が使えるパターンを作ると重複無しで6144個作られる' do
+      @solver.make_rpn_pattern_array(@rpn1.array).uniq.size.should == 6144
+    end
+  
   end
 
+  context "数字4つではない場合" do
 
-  it '逆ポーランド記法で解ける形で作られる最初のパターンと最後のパターンをテスト' do
-    @str.make_rpn_string_array[0].should == "1234+++"
-    @str.make_rpn_string_array[-1].should == "43/2/1/"
-  end
+    before do
+      @rpn2 = Rpn.new([1, 2, 3, 4, 5])
+      @rpn3 = Rpn.new([1, 2, 3])
+      @solver = PuzzleSolver.new
+    end
 
-  it '文字列が4つではない場合、エラーを返す' do
-    expect {@str1.calclate_rpn}.to raise_exception
-  end
+    it '文字列が4つではない場合、エラーを返す' do
+      expect {@solver.make_rpn_pattern_array(@rpn2.array)}.to raise_exception
+      expect {@solver.make_rpn_pattern_array(@rpn3.array)}.to raise_exception
+    end
 
-  it '4つの数字から逆ポーランド記法が使えるパターンを作ると重複無しで6144個作られる' do
-    @str.make_rpn_string_array.uniq.size.should == 6144
   end
 
 end
@@ -49,16 +82,17 @@ end
 describe "solve" do
   
   before do
-    @str = "1234"
-    @str1 = "1111"
+    @rpn = Rpn.new([1, 2, 3, 4])
+    @rpn1 = Rpn.new([1, 1, 1, 1])
+    @solver = PuzzleSolver.new
   end
 
   it "" do
-    @str.solve_puzzle(10)[0].should == "1234+++"
+    @solver.solve_puzzle(@rpn, 10)[0].should == [1, 2, 3, 4, "+", "+", "+"]
   end
 
   it "1111の数字からは10を作れないので配列は空で返される" do
-    @str1.solve_puzzle(10).should == []
+    @solver.solve_puzzle(@rpn1, 10).should == []
   end
 
 end
