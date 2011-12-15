b86c39a871f6cfb92facdcd8cd9938c70f58b6d3
yamano <yamano1023@gmail.com>
2011-12-15 11:23:14 +0900

    リファクタリングした

--- graph/gragh.rb
+++ graph/gragh.rb
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 class Gragh
-  def initialize(matrix, nodes)
-    @node_data = {}
+  def initialize(matrix = [], nodes = [])
+    # ノードのシンボルを渡せばクラスが返ってくるようにnodes_dataを初期化。
+    @nodes_data = {}
     nodes.each do |node|
-      @node_data[node] = Node.new(node)
+      @nodes_data[node] = Node.new(node)
     end
     
+    # 出発地と到着地ののシンボルを渡せば隣接時間が返ってくるようにtime_tableを初期化。
     @time_table = {}
     nodes.each.with_index do |origin_node, origin_index|
-      table  =  {}
+      table = {}
       nodes.each.with_index do |destination_node, destination_index|
         table[destination_node] = matrix[origin_index][destination_index]
       end
@@ -16,57 +18,59 @@ class Gragh
     end
 
   end
-  attr_accessor :node_data, :time_table
+  attr_accessor :nodes_data, :time_table
   
-  def solve_shortest_time(start_node, goal_node)
-    @node_data[start_node].transit_time = 0
-    #@node_data[start_node].course = [start_node]
-    update_transit_value(start_node, start_node, goal_node)
+  def solve_shortest_time(start_node)
+    # スタート地点の移動時間と経路を初期化。
+    @nodes_data[start_node].transit_time = 0
+    @nodes_data[start_node].course = [start_node]
+    # 次の
+    update(start_node, start_node)
   end
   
-  def update_transit_value(before_node, present_node, goal_node)
-    
-    @time_table[present_node].each_pair do |node, time|
-      unless @node_data[before_node].course.index(node)
-        unless time == 0
-          p node
-          p @node_data[node].transit_time = time + @node_data[before_node].transit_time
-          p @node_data[node].course = @node_data[before_node].course + [node]
-          #unless node == before_node
-            update_transit_value(present_node, node, goal_node)
-          #end
-        end
-      else
-      return @node_data[goal_node].transit_time if present_node == goal_node
-      end    
+  def update(prev_node, current_node)
+    @time_table[current_node].each_pair do |next_node, time|      
+      # 隣接しているnodeを探す。
+      unless time == 0
+        # 以前いたノードの経路に次のノードが含まれていないことを確認。
+        unless @nodes_data[prev_node].course.index(next_node)
+          new_transit_time = @nodes_data[current_node].transit_time + time
+          # 新しい移動時間のほうが短ければ移動時間と経路を更新。
+          if compare_transit_time(@nodes_data[next_node].transit_time, new_transit_time)
+            @nodes_data[next_node].transit_time = new_transit_time
+            @nodes_data[next_node].course = @nodes_data[current_node].course + [next_node]
+            update(current_node, next_node)
+          end
+        end  
+      end
     end
   end
-#  end
-  
-#  def check_distance
-    
-    
-#  end
-  
+
+  def compare_transit_time(old_transit_time, new_transit_time)
+    old_transit_time == nil || new_transit_time < old_transit_time
+  end
+
+
 end
 
 class Node
   def initialize(name)
-    @name  =  name
-    @transit_time = -1
+    @name = name
+    @transit_time
     @course = []
-    @check = 0
   end
-  attr_accessor :name, :transit_time, :course, :check
+  attr_accessor :name, :transit_time, :course
 end
 
 if $0 == __FILE__
-  matrix  =  [[0, 2, 0],
-              [2, 0, 3],
-              [0, 3, 0]]
+  matrix = [[ 0,  3, 10,  0],
+            [ 3,  0,  4, 15],
+            [10,  4,  0, 10],
+            [ 0, 15, 10,  0]]
   
-  stations  =  [:a, :b, :c]
+  stations = [:a, :b, :c, :d]
   gragh = Gragh.new(matrix, stations)
-  p gragh.solve_shortest_time(:a, :c)
-  p gragh.node_data[:c].course
+  gragh.solve_shortest_time(:a)
+  p gragh.nodes_data[:d].course
+  p gragh.nodes_data[:d].transit_time
 end
--- graph/gragh_spec.rb
+++ graph/gragh_spec.rb
@@ -3,40 +3,87 @@ require File.expand_path("gragh", File.dirname(__FILE__))
 
 describe "gragh" do
 
-  before do
-    matrix  = [[  0, 12, 28,  0, 0, 0],
-               [ 12,  0, 10, 13, 0, 0],
-               [ 28, 10,  0, 11, 7, 0],
-               [  0, 13, 11,  0, 0, 9],
-               [  0,  0,  7,  0, 0, 4],
-               [  0,  0,  0,  9, 4, 0]]
+  context "初期化テスト" do
 
-    stations  = [:yokohama, :musashikosugi, :shinagawa, :shibuya, :shinbashi, :tameikesannou]
+    before do
+      # matarixは隣接するノードとの移動時間を表している。
+      # 縦が出発地で横が到着地である。隣接していなければ0となる。
+      matrix = [[ 0,  3, 10,  0],
+                [ 3,  0,  4, 15],
+                [10,  4,  0, 10],
+                [ 0, 15, 10,  0]]
+      # nodesは各ノードのシンボルが格納。
+      nodes = [:a, :b, :c, :d] 
+      @gragh = Gragh.new(matrix, nodes)
+    end
 
-    @gragh = Gragh.new(matrix, stations)
+    it "nodes_data[:a]のtransit_timeに初期値であるnilが入っている" do
+      @gragh.nodes_data[:a].transit_time.should == nil
+    end
+    
+    it "nodes_data[:a]のcourseに初期値である[]が入っている" do
+      @gragh.nodes_data[:a].course.should == []
+    end
+
+    it "出発地と到着地から移動時間を出力。bからdまでは15が返ってくる" do
+      @gragh.time_table[:b][:d].should == 15
+    end
+      
   end
 
-  it "初期化テスト" do
-    @gragh.time_table[:yokohama][:shinagawa].should == 28    
+  context "各メソッドのテスト" do
+    
+    before do
+      @gragh2 = Gragh.new
+    end
 
+    it "新しい値が小さければtrue、大きければfalseが返る。また、古い値がnilでもtrueを返す。" do
+      @gragh2.compare_transit_time(10, 20).should == false
+      @gragh2.compare_transit_time(20, 10).should == true
+      @gragh2.compare_transit_time(nil, 10).should == true
+    end
 
-    @gragh.node_data[:yokohama].transit_time.should == -1
-    @gragh.node_data[:yokohama].course.should == []
   end
 
-  it "" do
-#    update_distance.should == nil
+  context "yokohamaをスタート地点にして各駅に最短経路で行ったときの移動時間と経路のテスト" do
+    
+    before do
+      matrix  = [[  0, 12, 28,  0, 0, 0],
+                 [ 12,  0, 10, 13, 0, 0],
+                 [ 28, 10,  0, 11, 7, 0],
+                 [  0, 13, 11,  0, 0, 9],
+                 [  0,  0,  7,  0, 0, 4],
+                 [  0,  0,  0,  9, 4, 0]]
+      stations  = [:yokohama, :musashikosugi, :shinagawa, :shibuya, :shinbashi, :tameikesannou]
+      
+      @gragh3 = Gragh.new(matrix, stations)
+      @gragh3.solve_shortest_time(:yokohama)
+    end
+    
+    it "yokohamaまで最短経路で行ったときの移動時間は22となる" do
+      @gragh3.nodes_data[:yokohama].transit_time.should == 0
+    end
+
+    it "yokohamaまでの最短経路はyokohama, musakikosugi, shinagawaとなる" do
+      @gragh3.nodes_data[:yokohama].course.should == [:yokohama]
+    end
+
+    it "shinagawaまで最短経路で行ったときの移動時間は22となる" do
+      @gragh3.nodes_data[:shibuya].transit_time.should == 25
+    end
+    
+    it "shinagawaまでの最短経路はyokohama, musakikosugi, shinagawaとなる" do
+    @gragh3.nodes_data[:shibuya].course.should == [:yokohama, :musashikosugi, :shibuya]
+    end
+      
+    it "tameikesannnouまで最短経路で行ったときの移動時間は33となる" do
+      @gragh3.nodes_data[:tameikesannou].transit_time.should == 33
+    end
+    
+    it "tameikesannnouまでの最短経路はyokohama, musakikosugi, shinagawa, shinbashi, tameikesannouとなる" do
+      @gragh3.nodes_data[:tameikesannou].course.should == [:yokohama, :musashikosugi, :shinagawa, :shinbashi, :tameikesannou]
+    end
+
   end
 
-  it "" do
-#    check_distance.should == nil
-  end
-
-  it "" do
-    @gragh.solve_shortest_time(:yokohama, :tameikesannou).should == 33
-  end
-  
-  it "" do
-    @gragh.node_data[:tameikesannou].course.should == [:yokohama, :musashikosugi, :shinagawa, :shibuya, :shinbashi, :tameikesannou]
-  end
 end
