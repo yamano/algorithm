2e47a272f25f203d061fc3fe04d91a0254a1e167
yamano <yamano1023@gmail.com>
2011-12-26 11:21:06 +0900

    自動反映スクリプト作成

--- script/tdiary/tdiary.rb
+++ script/tdiary/tdiary.rb
@@ -6,12 +6,13 @@ require 'kconv'
 class Tdiary
   
   def initialize(url)
-    @url =url
+    @url = url
+    @uuap_url 
     @agent = Mechanize.new
     @agent.get(url)
     @data
   end
-  attr_accessor :url, :agent, :data
+  attr_accessor :url, :uuap_url, :agent, :data
 
   def latest
     @agent.get(url)
@@ -20,13 +21,29 @@ class Tdiary
     latest_date[7] = "-"
     latest_date[10] = ""
 
+    latest_title = agent.page.at('h2').inner_text[15..-1]
+
     latest_body = agent.page.search('p')[0].inner_text[2..-1]
     
-    @data = { :date => latest_date, :body => latest_body}
+    @data = { :date => latest_date, :title => latest_title, :body => latest_body}
   end
 
   def has_new_diary
     @data != latest
+  end
+
+  def access_edit_display
+    uuap_url = "http://uuap.src.ricoh.co.jp/index.php?cmd=edit&page=%BB%B3%CC%EE%A4%CE%C6%FC%B5%AD%2F" + @data[:date] + "&refer=%BB%B3%CC%EE%A4%CE%C6%FC%B5%AD"
+    @agent.get(uuap_url)
+    @uuap_url = uuap_url
+  end
+
+  def add_body
+    p tmp = @agent.page.search("textarea")[1].inner_text
+    agent.page.form_with(:action => 'http://uuap.src.ricoh.co.jp/index.php'){|f|
+      f.field_with(:name => 'msg').value = tmp + "\n*" + @data[:title] + "-tdiary" + "[/#f7752111]\n" + @data[:body][0..40] + "・・・\n" + @url# + "?date=" + @data[:date][0..3] + @data[:date][5..6] + @data[:date][8..-1]
+      f.click_button(f.button_with(:value => "ページの更新"))
+    }
   end
 
 end
@@ -43,6 +60,8 @@ if $0 == __FILE__
       p "更新"
       p yamano.data[:date]
       p yamano.data[:body]
+      yamano.access_edit_display
+      yamano.add_body
     end
     sleep 1
 
--- script/tdiary/tdiary_spec.rb
+++ script/tdiary/tdiary_spec.rb
@@ -1,18 +1,27 @@
+# -*- coding: utf-8 -*-
 require File.expand_path("tdiary", File.dirname(__FILE__))
+
 
 describe "tdiary" do
 
   before do
     @yamano = Tdiary.new("http://yamano.nsp.ricoh.co.jp:8000/tdiary/")
   end
-
+  
   it "" do
-    @yamano.latest.should == { :date => "2011-12-21", :body => "test."}
+    @yamano.latest[:date].should == "2011-12-26"
+    @yamano.latest[:title].should == "自動反映スクリプト作成。"
   end
   
   it "" do
     @yamano.latest
     @yamano.has_new_diary.should == false
+  end
+  
+  it "" do
+    @yamano.latest
+    @yamano.access_edit_display
+    @yamano.uuap_url.should == "http://uuap.src.ricoh.co.jp/index.php?cmd=edit&page=%BB%B3%CC%EE%A4%CE%C6%FC%B5%AD%2F2011-12-26&refer=%BB%B3%CC%EE%A4%CE%C6%FC%B5%AD"
   end
 
 end
